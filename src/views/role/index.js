2d53ba59aa5ffccaa202d8382d032ba832297791
Asuddle <ahmad.suddle@gmail.com>
2021-11-22 02:28:23 +0500

    fic

--- src/views/role/index.js
+++ src/views/role/index.js
@@ -13,14 +13,54 @@ import {
   CTabs,
   CCol,
 } from "@coreui/react";
+
 import AsyncSelect from "src/reusable/asyncSelect";
+import SelectInput from "src/reusable/select";
 
 function RolePermissionComponent(props) {
   const [user, setUser] = useState("");
+  const [userType, setUserType] = useState("admin");
+  const [detailData, setDetailData] = useState({});
+  const [finalData, setFinalData] = useState({});
   const handleGetData = (val) => {
-    setUser(val);
+    console.log(val);
+    setDetailData(val);
   };
-  console.log("value here", user);
+  const handleUser = (val, val1) => {
+    setUser(val1);
+    console.log(val, val1);
+    console.log(detailData);
+    const temp = detailData.filter((item) => item.id == val1);
+    console.log("here is the temporary thing", temp);
+    setFinalData(temp[0]);
+  };
+  console.log(finalData);
+  let accountRegistrationArray = [
+    "End User's Registration",
+    "Empoyee's Registration",
+    "Dealer's Registration",
+  ];
+  let inventoryManagmentArray = [
+    "Single File Addition",
+    "Bulk File Addition",
+    "File Update (except price)",
+    "File Update (with price)",
+    "File Deletion",
+    "Single File Transfer",
+    "Bulk File transfer",
+    "Single File View",
+    "Files Grid View (assigned to login user)",
+    "Files Grid View (assigned to any user)",
+    "Assigned history (performed by login user)",
+    "Assigned history (performed by any user)",
+  ];
+  let userDataArr = [
+    { label: "Name", name: "name" },
+    { label: "CNIC", name: "CNIC" },
+    { label: "Email", name: "email" },
+    { label: "Phone Number", name: "phoneNo" },
+    { label: "Security Code", name: "securityCode" },
+  ];
   return (
     <div>
       <CCard>
@@ -33,35 +73,110 @@ function RolePermissionComponent(props) {
               <CLabel>
                 <strong>Select User Type </strong>{" "}
               </CLabel>
-              <AsyncSelect
+              <SelectInput
+                handleBlur={() => {}}
+                touched={""}
+                options={[
+                  { value: "admin", label: "Admin" },
+                  { value: "dealer", label: "Dealer" },
+                  { value: "endUser", label: "End User" },
+                ]}
+                value={userType}
+                setValue={(val1, type) => setUserType(type)}
+                name="fileType"
+              />
+              {/* <AsyncSelect
                 touched={""}
                 handleBlur={() => {}}
                 error={""}
                 value={user}
-                setValue={setUser}
-                getAllData={handleGetData}
                 name="user"
                 url={"admin"}
-              />
+              /> */}
             </CCol>
             <CCol>
               <CLabel>
                 <strong>Search User</strong>{" "}
               </CLabel>
-              <AsyncSelect
-                touched={""}
-                handleBlur={() => {}}
-                error={""}
-                value={user}
-                setValue={setUser}
-                name="user"
-                url={"admin"}
-              />
+              {(userType == "admin" || userType == "endUser") && (
+                <AsyncSelect
+                  touched={""}
+                  handleBlur={() => {}}
+                  error={""}
+                  value={user}
+                  getAllData={handleGetData}
+                  setValue={handleUser}
+                  name="user"
+                  url={"admin"}
+                />
+              )}
+              {userType == "dealer" && (
+                <AsyncSelect
+                  touched={""}
+                  handleBlur={() => {}}
+                  error={""}
+                  value={user}
+                  getAllData={handleGetData}
+                  setValue={handleUser}
+                  name="user"
+                  url={"dealer"}
+                />
+              )}
             </CCol>
           </CRow>
-          <table className="table table-striped table-hover">
-            <tbody></tbody>
-          </table>
+          <br />
+
+          {Object.entries(finalData).length > 0 && (
+            <>
+              <table className="table  table-hover">
+                <h4>Basic Information</h4>
+                {userDataArr.map((item, index) => (
+                  <tr key={index.toString()}>
+                    <td>{item.label}</td>
+                    <td>
+                      <strong>{finalData[item.name]}</strong>
+                    </td>
+                  </tr>
+                ))}
+              </table>
+              <table className="table table-striped table-hover">
+                <tbody>
+                  <h4>Account Registration</h4>
+                  {accountRegistrationArray.map((item, index) => (
+                    <tr key={index.toString()}>
+                      <td>{item}</td>
+                      <td>
+                        <input
+                          class="form-check-input"
+                          type="checkbox"
+                          defaultChecked
+                          value=""
+                          id="flexCheckDefault"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                  <br />
+                  <br />
+                  <h4>Inventory Managment </h4>
+                  {inventoryManagmentArray.map((item, index) => (
+                    <tr key={index.toString()}>
+                      <td>{item}</td>
+                      <td>
+                        <input
+                          class="form-check-input"
+                          type="checkbox"
+                          defaultChecked
+                          value=""
+                          id="flexCheckDefault"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
         </CCardBody>
       </CCard>
     </div>
